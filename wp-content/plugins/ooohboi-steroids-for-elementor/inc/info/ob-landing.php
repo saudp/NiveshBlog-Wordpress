29eabace5f4f04f210ff88251ed7643b1365af08
saudpds <saudpds@gmail.com>
2022-06-07 16:16:20 +0530

    wordpress initial commit

---
+++ wp-content/plugins/ooohboi-steroids-for-elementor/inc/info/ob-landing.php
@@ -0,0 +1,104 @@
+<div class="sfe-header">
+    <img src="<?php echo  SFE_DIR_URL . 'img/sfe-header.png'; ?>" alt="OoohBoi Steroids for Elementor" />
+</div>
+
+<div class="sfe-content">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-8">
+
+                <h3>What's new in v1.9.0?</h3>
+                <p>
+                <strong>Getting ready for a big change - the Elementor 3.6</strong>
+                </p>
+                <p>
+                All the extensions (but the TELEPORTER) are now Container element friendly which means that if you are one of Elementor beta-testers, you can enjoy the SFE on Elementor 3.6 as well. 
+                Of course, the BREAKING BAD extension will no longer be needed due to the fact that a new Container element includes all of its options by default. <br/>
+                More about the upcoming changes is yet to come. 
+                </p>
+                <p>
+                <strong>Major code improvements</strong>
+                </p>
+                <p>
+                The extensions code has been rewritten and now it gets loaded per Elementor template/page only if the particular extension is in use. That makes everything a lot faster. 
+                </p>
+                <p>
+                <strong>The GSAP library and its plugins</strong>
+                </p>
+                <p>
+                Due to the non-compliant WP license (GPLv2), the GSAP and all of its free plugins are now loaded from the CDN (cdnjs.com) instead of being part of the SFE. 
+                That should not have any performance impact or make your Elementor templates/pages that rely on these libs disfunctional in any way. 
+                </p>
+                <p>
+                <strong>Extensions revised</strong>
+                </p>
+                <p>
+                The extensions settings/options have been revised, minor glitches have been fixed, and the most important is the fact that all the "Reset to Default" now work properly.<br/>
+                As already mentioned, all of them are now Elementor 3.6 friendly so rest assured that everything will continue to work normaly once the big change happens. 
+                </p>
+                <p>
+                <strong>Interface improved</strong>
+                </p>
+                <p>
+                From now on, SFE includes support for the Light/Dark Elementor mode. Of course, the dark mode should be preferred/default one but the light mode has still been "refreshed" in the same fashion.
+                </p>
+                <p>
+                    <img src="<?php echo  SFE_DIR_URL . 'img/sfe-new-interface.png'; ?>" alt="SFE 1.9 new interface" />
+                </p>
+
+            </div>
+
+            <div class="col-4">
+
+                <h3>Sponsored by</h3>
+                <p>
+                    <img src="<?php echo  SFE_DIR_URL . 'img/RemPro.png'; ?>" alt="RemPro.nl" />
+                </p>
+                <p>
+                    <a href="mailto:oooh.boi29@gmail.com?subject=SFE%20Sponsorship">
+                    <img src="<?php echo  SFE_DIR_URL . 'img/sponsor-needed.png'; ?>" alt="Sponsor needed" />
+                    </a>
+                </p>
+
+                <hr />
+
+                <h3>Resources</h3>
+                <p>
+                Want to learn more about this plugin? Here's a list of tutorials on YouTube:
+                </p>
+                <ul>
+                    <li><a href="https://youtu.be/ccGpcvi-t4A" title="GSAP + Scroll Trigger + Locomotive Scroll" target="_blank">GSAP + Scroll Trigger + Locomotive Scroll</a></li>
+                    <li><a href="https://youtu.be/dG2uumcwX7s" title="The Smooth, Locomotive Scroll" target="_blank">The Smooth, Locomotive Scrolld</a></li>
+                    <li><a href="https://youtu.be/Vp8O_uVWD-c" title="The Hover Animator explained" target="_blank">The Hover Animator explained</a></li>
+                    <li><a href="https://youtu.be/JpRTDaJQQvE" title="Widget Stalker" target="_blank">How to optimize the layout in Elementor - Widget Stalker</a></li>
+                    <li><a href="https://youtu.be/WRPu1v4Kh9g" title="Masked Video Background" target="_blank">Masked Video Background - Videomasq</a></li>
+                    <li><a href="https://youtu.be/qS8lCzNhTbE" title="Teleporter Hero" target="_blank">Teleporter Hero</a></li>
+                    <li><a href="https://youtu.be/9t4vugF2Lok" title="Glider" target="_blank">Glider - Custom Content Slider for Elementor</a></li>
+                </ul>
+                <p class="small-text"> More videos will be added soon!</p>
+
+                <hr />
+
+                <h3>Donations</h3>
+                <p>
+                If you think that this plugin helps your business, please consider a small donation to support further development. Thank you! 
+                </p>
+                <p>
+                    <a href="https://www.paypal.me/ooohboi" title="Donate with PayPal" target="_blank"><img src="<?php echo  SFE_DIR_URL . 'img/paypal-donate.png'; ?>" alt="Donate with PayPal" class="pp" /></a>
+                </p>
+
+            </div>
+
+            <div class="col-12">
+                <hr />
+                <p class="small-text">Current version: <?php echo OoohBoi_Steroids::VERSION; ?></p>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
---
+++ wp-content/themes/hello-elementor/template-parts/header.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying header.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+$site_name = get_bloginfo( 'name' );
+$tagline   = get_bloginfo( 'description', 'display' );
+$header_nav_menu = wp_nav_menu( [
+	'theme_location' => 'menu-1',
+	'fallback_cb' => false,
+	'echo' => false,
+] );
+?>
+<a class="skip-link screen-reader-text" href="#content">
+	<?php _e( 'Skip to content', 'hello-elementor' ); ?></a>
+
+<div className="container">
+    <div className="Navmenu">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </div>
+    </div>
